9f8c8e1bf5aa9d57ea44a6d7f0b4efd6102acc34
Razshy <88072010+Razshy@users.noreply.github.com>
2023-03-13 16:05:38 -0600

    Update index.js

--- public/index.js
+++ public/index.js
@@ -22,11 +22,7 @@ function getSite() {
         const guid2 = items[2].getElementsByTagName("guid")[0].textContent;
         const guid3 = items[3].getElementsByTagName("guid")[0].textContent;
   
-        const link0 = items[0].getElementsByTagName("link")[0].textContent;
-        const link1 = items[1].getElementsByTagName("link")[0].textContent;
-        const link2 = items[2].getElementsByTagName("link")[0].textContent;
-        const link3 = items[3].getElementsByTagName("link")[0].textContent;
-  
+
         const id0 = guid0.substring(13);
         const id1 = guid1.substring(13);
         const id2 = guid2.substring(13);
@@ -41,16 +37,6 @@ function getSite() {
         document.getElementById(`title1`).href = `/inscription?inscriptionid=${id1}`;
         document.getElementById(`title2`).href = `/inscription?inscriptionid=${id2}`;
         document.getElementById(`title3`).href = `/inscription?inscriptionid=${id3}`;
-  
-        document.getElementById("guid0").innerHTML = `${guid0}`;
-        document.getElementById("guid1").innerHTML = `${guid1}`;
-        document.getElementById("guid2").innerHTML = `${guid2}`;
-        document.getElementById("guid3").innerHTML = `${guid3}`;
-  
-        document.getElementById("link0").innerHTML = `${link0}`;
-        document.getElementById("link1").innerHTML = `${link1}`;
-        document.getElementById("link2").innerHTML = `${link2}`;
-        document.getElementById("link3").innerHTML = `${link3}`;
   
         document.getElementById("img0").src=`https://ordinals.com/content/${id0}`;
         document.getElementById("img1").src=`https://ordinals.com/content/${id1}`;
